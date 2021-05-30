4b0f099793bbacf0482d9b5c0545ca732785bf9d
Justin Lattimore <jlattimoreweb@gmail.com>
2021-05-30 18:17:03 -0500

    committing current app as it is

---
+++ README.md
@@ -0,0 +1,17 @@
+# RF Code assessment
+
+### PrintStation program
+
+#### Details
+- node v14.15.5
+- installed [nodemon](https://www.npmjs.com/package/nodemon) for development
+
+#### Instructions to begin program
+
+1) Run in command line:
+
+`yarn install`
+
+2) Then run
+
+`yarn run start`
--- index.js
+++ index.js
@@ -1,35 +1,64 @@
 const inquirer = require('inquirer')
 
 class PrintStation{
-  constructor(size, labelCount){
-    this.size = size
-    this.labelCount = labelCount
+  constructor(){
+    this.buffer = 0
     this.maxCharacters = 1024
+    this.smallCharCount = 75
+    this.mediumCharCount = 175
+    this.largeCharCount = 375
   }
 
-  //Output method for entire app
-  output = () => {
-    console.log(`Size input is "${this.size}" and you want "${this.labelCount}"`);
+  //Start method for entire app
+  start(){
+    this.welcome()
   }
+
+  clearDashboard = (ui) => {
+    console.clear()
+  }
+
+  welcome(){
+    this.clearDashboard()
+    console.log(`================= Welcome to the printer =====================`)
+    console.log(`Input the number of boxes that you have`)
+
+    this.getBoxCount()
+  }
+
+  getBoxCount(){
+    var questions = [
+      {type: 'input', name: 'SmallBoxes', message: "How many small boxes?"},
+      {type: 'input', name: 'MediumBoxes', message: "How many medium boxes?"},
+      {type: 'input', name: 'LargeBoxes', message: "How many large boxes?"}
+    ]
+
+    inquirer
+      .prompt(questions)
+      .then((answers) => {
+        this.printInit(answers["SmallBoxes"], answers["MediumBoxes"], answers["LargeBoxes"])
+      })
+      .catch((error) => console.log(error))
+    }
+
+    printInit(smallCount, mediumCount, largeCount){
+      this.clearDashboard()
+      console.log(`===================== Printing.... ===========================`)
+      console.log(`Printing labels for ${smallCount} small, ${mediumCount} medium, ${largeCount} large boxes!`)
+      console.log(`--------------------------------------------------------------`)
+      let parsePrintJob = this.getCharCount(smallCount, mediumCount, largeCount)
+      console.log(`This will take ${parsePrintJob.time} seconds with ${parsePrintJob.charCount} characters and a total of ${Math.ceil(parsePrintJob.pageCount)} pages`)
+
+    }
+
+    getCharCount(small, medium, large){
+      let charCount = ((small * this.smallCharCount) + (medium * this.mediumCharCount) + (large * this.largeCharCount))
+      let time = Math.floor(charCount / 150)
+      let pageCount = charCount / 1024
+      return {time, charCount, pageCount}
+    }
+
 }
 
-var questions = [
-  {
-    type: 'input',
-    name: 'Label size',
-    message: "What is the size?"
-  },
-  {
-    type: 'input',
-    name: 'Label count',
-    message: "How many?"
-  }
-]
-
-inquirer
-  .prompt(questions)
-  .then((answers) => {
-    var ps = new PrintStation(answers["Label size"], answers["Label count"]);
-    ps.output()
-  })
-  .catch((error) => console.log(error))
+var ps = new PrintStation();
+ps.start()
